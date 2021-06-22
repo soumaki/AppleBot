1d241acb3c127cc330f6cce6f5be9ff97d361488
Apple <scarotrhy@gmail.com>
2021-06-22 00:45:26 -0300

    @applled

--- userge/plugins/tools/search.py
+++ userge/plugins/tools/search.py
@@ -11,18 +11,18 @@ from userge import Message, userge
 
 @userge.on_cmd(
     "s",
-    about={"header": "search commands in USERGE", "examples": "{tr}s wel"},
+    about={"header": "Pesquises comandos no AppleBot", "exemplo": "{tr}s online"},
     allow_channels=False,
 )
 async def search(message: Message):
     cmd = message.input_str
     if not cmd:
-        await message.err(text="Enter any keyword to search in commands")
+        await message.err(text="Digite qualquer palavra para pesquisar")
         return
     found = [i for i in sorted(list(userge.manager.enabled_commands)) if cmd in i]
     out_str = "    ".join(found)
     if found:
-        out = f"**--I found ({len(found)}) commands for-- : `{cmd}`**\n\n`{out_str}`"
+        out = f"**--Eu encontrei ({len(found)}) comandos para sua pesquisa -- : `{cmd}`**\n\n`{out_str}`"
     else:
-        out = f"__command not found for__ : `{cmd}`"
+        out = f"__não encontrei nenhum comando da sua pesquisa__ : `{cmd}`"
     await message.edit(text=out, del_in=0)

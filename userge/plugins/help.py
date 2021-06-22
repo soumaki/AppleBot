05f26317e0bcf2688e605fe60762aad7198355b2
Apple <scarotrhy@gmail.com>
2021-06-22 05:02:15 -0300

    @applled

--- userge/plugins/help.py
+++ userge/plugins/help.py
@@ -1178,10 +1178,7 @@ if userge.has_bot:
 
             MAIN_MENU = InlineQueryResultArticle(
                 title="Menu Principal",
-                input_message_content=InputTextMessageContent(
-                    " Bem vindo ao\n AppleBot 𝐌𝐄𝐍𝐔 "
-                ),
-                photo_url="https://telegra.ph/file/a1876d2c20937b9e5e78e.png",
+                input_message_content=InputTextMessageContent(" Bem vindo ao\n AppleBot 𝐌𝐄𝐍𝐔 "),
                 url="https://t.me/applled",
                 description="Menu Principal",
                 thumb_url="https://telegra.ph/file/c253bbfab1a1a2e79cd9b.png",

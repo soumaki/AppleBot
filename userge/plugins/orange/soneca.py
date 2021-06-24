4367e44c7003ebe12ef3a7712861741bb1f3afc0
Apple <scarotrhy@gmail.com>
2021-06-24 01:52:48 -0300

    fix

--- userge/plugins/orange/soneca.py
+++ userge/plugins/orange/soneca.py
@@ -5,11 +5,12 @@ import time
 from random import randint
 from re import compile as comp_regex
 
+
 from userge import Config, Message, filters, get_collection, userge
 from userge.utils import time_formatter
 
 _TELE_REGEX = comp_regex(
-    r"http[s]?://(telegra\.ph/file|t\.me)/(\w+)(?:\.|/)(gif|jpg|png|jpeg|mp4|[0-9]+)(?:/([0-9]+))?"
+    r"http[s]?://(i\.imgur\.com|telegra\.ph/file|t\.me)/(\w+)(?:\.|/)(gif|jpg|png|jpeg|[0-9]+)(?:/([0-9]+))?"
 )
 TL = comp_regex(r"[<].*[>]")
 

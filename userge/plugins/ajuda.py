7d6125d1c4f1f63f8fe3cba09b77a249f57c581a
Apple <scarotrhy@gmail.com>
2021-06-27 20:51:01 -0300

    Update ajuda.py

--- userge/plugins/ajuda.py
+++ userge/plugins/ajuda.py
@@ -38,7 +38,7 @@ from .bot.utube_inline import (
 )
 from .fun.stylish import Styled, font_gen
 from .misc.redditdl import reddit_thumb_link
-from .orange.afk import _afk_
+from .custom.afk import _afk_
 from .utils.notes import get_inote
 
 CHANNEL = userge.getCLogger(__name__)

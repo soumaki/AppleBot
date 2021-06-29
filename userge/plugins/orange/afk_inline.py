9fef95d1c3572dcc9cb87dfa0e499973fdd36065
samuca78 <81726687+samuca78@users.noreply.github.com>
2021-06-29 13:51:25 -0300

    Add files via upload

---
+++ userge/plugins/orange/afk_inline.py
@@ -0,0 +1,25 @@
+from userge import Message, userge
+
+
+async def send_inline_afk(message: Message):
+    bot = await userge.bot.get_me()
+    x = await userge.get_inline_bot_results(bot.username, "afk")
+    await userge.send_inline_bot_result(
+        chat_id=message.chat.id, query_id=x.query_id, result_id=x.results[0].id
+    )
+
+
+async def send_inline_afk_(message: Message):
+    bot_ = await userge.bot.get_me()
+    x_ = await userge.get_inline_bot_results(bot_.username, "afk_")
+    await userge.send_inline_bot_result(
+        chat_id=message.chat.id, query_id=x_.query_id, result_id=x_.results[0].id
+    )
+
+
+async def _send_inline_afk(message: Message):
+    _bot = await userge.bot.get_me()
+    _x = await userge.get_inline_bot_results(_bot.username, "_afk")
+    await userge.send_inline_bot_result(
+        chat_id=message.chat.id, query_id=_x.query_id, result_id=_x.results[0].id
+    )

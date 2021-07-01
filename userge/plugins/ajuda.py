4c28481d9650913ef764a7e8745e50db3f5a8dd7
samuca78 <81726687+samuca78@users.noreply.github.com>
2021-06-30 21:52:47 -0300

    Update ajuda.py

--- userge/plugins/ajuda.py
+++ userge/plugins/ajuda.py
@@ -36,7 +36,8 @@ from .bot.utube_inline import (
     result_formatter,
     ytsearch_data,
 )
-from .custom.afk import _afk_
+# from .custom.afk import _afk_
+from .orange.afk import _afk_
 from .fun.stylish import Styled, font_gen
 from .misc.redditdl import reddit_thumb_link
 from .utils.notes import get_inote

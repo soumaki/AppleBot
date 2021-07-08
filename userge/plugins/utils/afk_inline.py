d2ea3e1846cee43f72c8723c08b39bbaedaf47ff
applled <applled@github.com>
2021-07-08 09:18:58 +0000

    auto: @applled

--- userge/plugins/utils/afk_inline.py
+++ userge/plugins/utils/afk_inline.py
@@ -5,7 +5,10 @@ async def send_inline_afk(message: Message):
     bot = await userge.bot.get_me()
     x = await userge.get_inline_bot_results(bot.username, "afk")
     await userge.send_inline_bot_result(
-        chat_id=message.chat.id, query_id=x.query_id, result_id=x.results[0].id, hide_via=True,
+        chat_id=message.chat.id,
+        query_id=x.query_id,
+        result_id=x.results[0].id,
+        hide_via=True,
     )
 
 
@@ -13,7 +16,10 @@ async def send_inline_afk_(message: Message):
     bot_ = await userge.bot.get_me()
     x_ = await userge.get_inline_bot_results(bot_.username, "afk_")
     await userge.send_inline_bot_result(
-        chat_id=message.chat.id, query_id=x_.query_id, result_id=x_.results[0].id, hide_via=True,
+        chat_id=message.chat.id,
+        query_id=x_.query_id,
+        result_id=x_.results[0].id,
+        hide_via=True,
     )
 
 
@@ -21,5 +27,8 @@ async def _send_inline_afk(message: Message):
     _bot = await userge.bot.get_me()
     _x = await userge.get_inline_bot_results(_bot.username, "_afk")
     await userge.send_inline_bot_result(
-        chat_id=message.chat.id, query_id=_x.query_id, result_id=_x.results[0].id, hide_via=True,
+        chat_id=message.chat.id,
+        query_id=_x.query_id,
+        result_id=_x.results[0].id,
+        hide_via=True,
     )

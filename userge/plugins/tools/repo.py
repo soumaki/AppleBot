bdebf1b04a9cdc9accb057e928702f0bed051f40
Apple <scarotrhy@gmail.com>
2021-07-10 00:39:42 -0300

    @applled

--- userge/plugins/tools/repo.py
+++ userge/plugins/tools/repo.py
@@ -1,13 +1,4 @@
-# Copyright (C) 2020 by UsergeTeam@Github, < https://github.com/UsergeTeam >.
-#
-# This file is part of < https://github.com/UsergeTeam/Userge > project,
-# and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/uaudith/Userge/blob/master/LICENSE >
-#
-# All rights reserved.
-
 from userge import Config, Message, userge
-
 
 @userge.on_cmd("repo", about={"header": "Link do Repo"})
 async def see_repo(message: Message):

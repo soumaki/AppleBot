3a385b3e7cd633765aa364c834645b4197244e5c
applled <applled@github.com>
2021-07-10 03:41:53 +0000

    auto: @applled

--- userge/plugins/tools/repo.py
+++ userge/plugins/tools/repo.py
@@ -1,12 +1,13 @@
 from userge import Config, Message, userge
 
+
 @userge.on_cmd("repo", about={"header": "Link do Repo"})
 async def see_repo(message: Message):
-    """ Repo do AppleBot """    
+    """Repo do AppleBot"""
     photo = "https://telegra.ph/file/a1876d2c20937b9e5e78e.png"
     texto = f"• **Tchamram!**: [AppleBot]({Config.UPSTREAM_REPO})"
     await message.client.send_animation(
-                         message.chat.id, 
-                         animation=photo, 
-                         caption=texto,
+        message.chat.id,
+        animation=photo,
+        caption=texto,
     )

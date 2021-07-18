5e9458bcb98f5cf0087c83fd3280d59e404b7110
Apple <scarotrhy@gmail.com>
2021-07-18 19:10:06 -0300

    @applled

--- userge/plugins/orange/goo.py
+++ userge/plugins/orange/goo.py
@@ -1,11 +1,7 @@
 """Pesquisa simplificada do Google - @applled"""
 
-import random
-
 import requests
-
 from userge import Message, userge
-
 
 @userge.on_cmd(
     "goo",
@@ -15,15 +11,22 @@ from userge import Message, userge
     },
 )
 async def goo_(message: Message):
-    """Google ;)"""
+    """goo_"""
     query = message.input_or_reply_str
     if not query:
         await message.edit("`Vou pesquisar o vento?!`")
         return
     query_encoded = query.replace(" ", "+")
+    #   query_encoded = query2.replace(" ", "+")
     goo_url = f"https://www.google.com/search?q={query_encoded}"
+    #   twt_url = f"https://twitter.com/search?q={query2_encoded}"
     payload = {"format": "json", "url": goo_url}
     r = requests.get("http://is.gd/create.php", params=payload)
-   await message.edit(
-        f"✅ **Este é o resultado da Sua Pesquisa no Google:\n🔗 [{query}]({r.json()['shorturl']})\n\n ➖➖➖➖\nDev: @applled"
-   )
+    await message.edit(
+        f"""
+✅ **Este é o resultado da Sua Pesquisa no Google:**
+🔗 [{query}]({r.json()['shorturl']})
+  ➖➖➖➖
+Dev: @applled
+"""
+    )

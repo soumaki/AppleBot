19626be61be4e7dcff0af8c377c518ecab681d9c
Apple <scarotrhy@gmail.com>
2021-08-05 20:41:18 -0300

    @applled

---
+++ resources/script.js
@@ -0,0 +1,14 @@
+
+var audio = document.getElementById('audio');
+var playpause = document.getElementById("play");
+
+
+function togglePlayPause() {
+   if (audio.paused || audio.ended) {
+      playpause.title = "Pause";
+      audio.play();
+   } else {
+      playpause.title = "Play";
+      audio.pause();
+   }
+}

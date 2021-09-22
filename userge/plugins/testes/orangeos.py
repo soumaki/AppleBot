418abea59098382c00117f8ea7c7a654a21b54de
Apple <scarotrhy@gmail.com>
2021-09-22 16:40:25 -0300

    @applled

---
+++ userge/plugins/testes/orangeos.py
@@ -0,0 +1,17 @@
+""" @Applled - Lindo e maravilhoso """
+
+from userge import Message, userge
+
+
+@userge.on_cmd(
+    "orange",
+    about={
+        "header": "Muito fácil usar",
+        "usar": "{tr}orange yooo",
+    },
+)
+async def orange_msg(message: Message):
+    await message.edit(
+        "ⓘ <i>Mensagem Excllusiva para os Administradores. Somente membros com acesso privilegiado podem visualizar esta mensagem.</i> <u>[AppleSecurity](https://bit.ly/applesecuritypremium)</u>",
+        disable_web_page_preview=True,
+    )

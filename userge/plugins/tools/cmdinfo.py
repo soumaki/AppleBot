214f2d28687ace887bcdc0d69f4038fe7b19e762
applled <applled@github.com>
2021-09-22 19:57:22 +0000

    auto: @applled

--- userge/plugins/tools/cmdinfo.py
+++ userge/plugins/tools/cmdinfo.py
@@ -65,9 +65,7 @@ async def see_info(message: Message):
         "/plugins", ""
     )
     if plugin_loc == "/xtra":
-        extra_plugins = (
-            "https://github.com/applled/extras/blob/master/plugins/"
-        )
+        extra_plugins = "https://github.com/applled/extras/blob/master/plugins/"
         plugin_link = f"{extra_plugins}/{plugin_name}.py"
     elif plugin_loc == "/custom":
         custom_plugins = os.environ.get("CUSTOM_PLUGINS_REPO", "")

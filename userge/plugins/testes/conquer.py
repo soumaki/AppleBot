fc8a3048fc027c214bc08c97f667c8c218c200ef
Apple <scarotrhy@gmail.com>
2021-09-22 16:07:56 -0300

    @applled

--- userge/plugins/testes/conquer.py
+++ userge/plugins/testes/conquer.py
@@ -5,7 +5,7 @@ import random
 from userge import Message, userge
 
 @userge.on_cmd("con$", about={"título": "Tu sabe o que é :D"}, trigger="", allow_via_bot=False)
-async def proton_msg(message: Message):
+async def conquer_msg(message: Message):
     await message.edit(
         "ⓘ <i>Exclusive Message for ConquerOS Premium. Only Private Premium User can view the this message.</i> <u>[More Details](https://bit.ly/conquerpremium)</u>", disable_web_page_preview=True)
   

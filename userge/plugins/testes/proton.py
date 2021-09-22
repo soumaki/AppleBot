3b52a62bc8c404856954d34bb3bd8bdea5983552
applled <applled@github.com>
2021-09-22 19:03:19 +0000

    auto: @applled

--- userge/plugins/testes/proton.py
+++ userge/plugins/testes/proton.py
@@ -1,12 +1,13 @@
 """ @Applled - Lindo e maravilhoso """
 
-import asyncio
-import random
 from userge import Message, userge
 
-@userge.on_cmd("pro$", about={"título": "Tu sabe o que é :D"}, trigger="", allow_via_bot=False)
+
+@userge.on_cmd(
+    "pro$", about={"título": "Tu sabe o que é :D"}, trigger="", allow_via_bot=False
+)
 async def proton_msg(message: Message):
     await message.edit(
-        "ⓘ <i>Essa mensagem está disponível apenas para usuários da ProtonAOSP Premium.</i> <u>[Saiba mais](https://bit.ly/protonpremium)</u>", disable_web_page_preview=True)
-  
- 
+        "ⓘ <i>Essa mensagem está disponível apenas para usuários da ProtonAOSP Premium.</i> <u>[Saiba mais](https://bit.ly/protonpremium)</u>",
+        disable_web_page_preview=True,
+    )

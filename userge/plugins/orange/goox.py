885bc09d2a39d561cc8f4dc24fe1fb893efc688e
Apple <scarotrhy@gmail.com>
2021-09-23 02:47:33 -0300

    @applled  revertido

--- userge/plugins/orange/goox.py
+++ userge/plugins/orange/goox.py
@@ -1,87 +1,43 @@
 """ Manter os créditos | @applled - Módulo que gera um adesivo animado de pesquisa do Google"""
 
-from pyrogram.errors import YouBlockedUser
-
+import random
+from pyrogram.errors import BadRequest
 from userge import Message, userge
-from userge.utils.exceptions import StopConversation
-
 
 @userge.on_cmd(
     "gx",
     about={
-        "titulo": "Módulo que gera um adesivo animado de pesquisa do Google",
-        "flags": {"-l": "limite, para várias mensagens"},
-        "como usar": "Responda {tr}t -l[limite de mensagem]",
-        "exemplo": ["{tr}t", "{tr}t -15"],
+        "header": "Módulo criado pelo @applled que gera um adesivo animado de pesquisa do Google",
+        "como usar": "{tr}gx amo maçãs",
     },
-    allow_via_bot=False,
     del_pre=True,
+    allow_channels=False,
 )
-async def tweetando(message: Message):
-    """Pesquisa de burro no Google - @applled"""
+async def appled_(message: Message):
     reply = message.reply_to_message
-    quote_list = []
-    self_mid = False
-    args = ""
-    if reply:
-        if "l" in message.flags:
-            limit = message.flags.get("l", 1)
-            if not limit.isdigit():
-                await message.err("Eu preciso de um texto, não de ventos.", del_in=5)
-                return
-            num_ = min(int(limit), 24)
-            async for msg in userge.iter_history(
-                message.chat.id, limit=num_, offset_id=reply.message_id, reverse=True
-            ):
-                if msg.message_id != message.message_id:
-                    quote_list.append(msg.message_id)
-            if message.filtered_input_str:
-                self_mid = True
-                await message.edit(message.filtered_input_str)
-        else:
-            quote_list.append(reply.message_id)
-            if message.input_str:
-                self_mid = True
-                await message.edit(message.input_str)
-    else:
-        args = message.input_str
-    if self_mid:
-        quote_list.append(message.message_id)
-    else:
-        await message.delete()
-    if not args and len(quote_list) == 0:
-        await message.err("Responda uma mensagem ou faça o comando + texto.", del_in=5)
-        return
+    reply_id = reply.message_id if reply else None
+    if message.input_str:
+        input_query = message.input_str
+    elif reply:
+        if reply.text:
+            input_query = reply.text
+        elif reply.caption:
+            input_query = reply.caption
+    if not input_query:
+        return await message.err("Lembre-se de fazer o comando + texto de pesquisa", del_in=5)
+
+    x = await userge.get_inline_bot_results("@GooglaxBot", input_query)
     try:
-        async with userge.conversation("googlaxbot ", timeout=100) as conv:
-            try:
-                if quote_list:
-                    await userge.forward_messages(
-                        "googlaxbot ", message.chat.id, quote_list
-                    )
-                    if self_mid:
-                        await message.delete()
-                elif args:
-                    await conv.send_message(args)
-            except YouBlockedUser:
-                await message.edit("Dê /start no @googlaxbot ")
-                return
-            quote = await conv.get_response(mark_read=True)
-            if not (quote.sticker or quote.document):
-                await message.err("Algo de certo está errado.")
-                return
-            message_id = reply.message_id if reply else None
-            if quote.sticker:
-                await userge.send_sticker(
-                    chat_id=message.chat.id,
-                    sticker=quote.sticker.file_id,
-                    reply_to_message_id=message_id,
-                )
-            else:
-                await userge.send_document(
-                    chat_id=message.chat.id,
-                    document=quote.document.file_id,
-                    reply_to_message_id=message_id,
-                )
-    except StopConversation:
-        await message.err("@googlaxbot ficou sem resposta.\nTente novamente.", del_in=5)
+        await message.delete()
+        await userge.send_inline_bot_result(
+            chat_id=message.chat.id,
+            query_id=x.query_id,
+            result_id=x.results[0].id,
+            reply_to_message_id=reply_id,
+            hide_via=True,
+        )
+    except (IndexError, BadRequest):
+        await message.err(
+            "Não é assim que funciona, jovem...",
+            del_in=5,
+        )

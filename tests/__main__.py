65ca17ddcf9ce815f648fa5cb10d7a9a542160a3
Apple <scarotrhy@gmail.com>
2021-09-30 13:30:30 -0300

    @applled

--- tests/__main__.py
+++ tests/__main__.py
@@ -16,8 +16,8 @@ from userge import userge
 async def _worker() -> None:
     chat_id = int(os.environ.get("CHAT_ID") or 0)
     type_ = 'unofficial' if os.path.exists("../userge/plugins/unofficial") else 'main'
-    await userge.send_message(chat_id, f'`{type_} build completed !`')
+    await userge.send_message(chat_id, f'`{type_} build completa!`')
 
 if __name__ == "__main__":
     userge.begin(_worker())
-    print('AppleBot test has been finished!')
+    print('Teste interno do AppleBot encerrado!')

bf812152676f871e8e365779c19692ce47fd7a86
applled <applled@github.com>
2022-05-17 17:54:56 +0000

    auto: @applled

--- userge/plugins/admin/fbans.py
+++ userge/plugins/admin/fbans.py
@@ -198,7 +198,7 @@ async def fban_(message: Message):
         for i in failed:
             status += "• " + i + "\n"
     else:
-        status = f"Success! Fbanned in `{total}` feds."  # 
+        status = f"Success! Fbanned in `{total}` feds."  #
     msg_ = (
         fban_arg[3].format(u_link)
         + f"\n**ID:** <code>{u_id}</code>\n**Reason:** {reason}\n**Status:** {status}"
